76136c96cbe39e1185cce11715c2f1f002f5e312
pedrohenriquelimaifpi <caang.2023119isinf0021@aluno.ifpi.edu.br>
2025-12-03 21:35:47 -0300

    4 Questão.cpp

---
+++ 4 Questão.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+    int num[5];
+
+    for(int i = 0; i < 5; i++){
+        cout << "Digite os 5 números inteiros para armazenamento: " << endl;
+        cin >> num[i]; 
+    }
+         cout << "O números digitados foram: " << endl;
+    for (int i =0 ;i <5; i++){
+        cout << num[i] << endl;
+    }
+   cout <<"A soma dos valores é: " << soma << endl;
+
+}
